95124cacd666236e36b7fb6c21234833c790aa39
choiyoungjin <ip951122@naver.com>
2022-03-12 19:48:02 +0900

    0312

---
+++ java_codingtest/src/baekjoon/baek2110.java
@@ -0,0 +1,62 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class baek2110 {
+
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		int[] list = new int[N];
+		
+	
+		for(int i=0;i<N;i++) {
+			list[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(list);//배열 정렬
+		
+		int low = 0;
+		int high =list[N-1]; //최대값 배열 끝 값
+		int mid=0;
+		
+		while(low<high) {
+			mid = low+(high-low+1)/2; //mid 값 중 최대값 찾아야 함 
+			
+			
+			int cur =list[0];
+			int cnt=1;
+			for(int i=1;i<N;i++) {
+				if(list[i]-cur <mid)
+					continue;
+				else {
+					cur=list[i];
+					cnt++;
+				}
+			}
+			if(cnt <C) {
+				high = mid-1;
+			}
+			else {
+				low = mid;		//최대값 찾기 떄문에 low는 범위에 포함시킴
+			}
+			
+		}
+		System.out.println(low);
+		
+	}
+
+}
