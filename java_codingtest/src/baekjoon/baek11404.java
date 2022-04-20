a45fc5dd9044fb6deaf543d0943f2b1a3f16bd31
choiyoungjin <ip951122@naver.com>
2022-04-21 00:01:42 +0900

    0421

---
+++ java_codingtest/src/baekjoon/baek11404.java
@@ -0,0 +1,74 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class baek11404 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb;
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());//정점 개수
+		int E = Integer.parseInt(br.readLine());//간선 개수
+
+		int[][] map = new int[N][N];
+
+		final int INF = 999_999_999; 
+
+		for(int i=0;i<N;i++)
+			for(int j=0;j<N;j++)
+				map[i][j] =INF;
+
+		for(int i=0;i<N;i++)
+			map[i][i]=0;
+
+		for(int i=0;i<E;i++) {
+			st= new StringTokenizer(br.readLine());
+
+			int from = Integer.parseInt(st.nextToken())-1;
+			int to = Integer.parseInt(st.nextToken())-1;
+			int val = Integer.parseInt(st.nextToken());
+			
+			if(map[from][to]==INF)
+				map[from][to]=val;
+			else if(map[from][to]>val)
+				map[from][to]=val;
+		}
+
+
+		for(int k=0;k<N;k++) {
+			for(int i=0;i<N;i++) {
+				if(k==i) continue;
+				for(int j=0;j<N;j++) {
+					if(i==j || k==j) continue;
+
+					if(map[i][j]> map[i][k]+map[k][j])
+						map[i][j] = map[i][k]+map[k][j];
+				}
+			}
+		}
+
+
+		for(int i=0;i<N;i++) {
+			sb = new StringBuilder();
+			for(int j=0;j<N;j++) {
+				sb.append(map[i][j]).append(" ");
+			}
+			bw.write(sb.toString().trim());
+			bw.write("\n");
+		}
+		bw.flush();
+		bw.close();
+
+	}
+
+}
