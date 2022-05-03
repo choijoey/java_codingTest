d7df2c4eea0f6bc624eb4c340ff69a0c43e2281b
choiyoungjin <ip951122@naver.com>
2022-05-04 00:35:00 +0900

    0504

--- java_codingtest/src/Main.java
+++ java_codingtest/src/Main.java
@@ -3,50 +3,36 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int test1[] = {1,2,2,3,3,3,4,6,7};
-		System.out.println(lowerBound(test1, 4));
-		System.out.println(upperBound(test1, 4));
-		
-		System.out.println(lowerBound(test1, 3));
-		System.out.println(upperBound(test1, 3));
-		
-		System.out.println(lowerBound(test1, 5));
-		System.out.println(upperBound(test1, 5));
-		
-		System.out.println(lowerBound(test1, 8));
-		System.out.println(upperBound(test1, 8));
-		
-		System.out.println(lowerBound(test1, 0));
-		System.out.println(upperBound(test1, 0));
+
 
 	}
 
-	   public static int lowerBound(int[] array,  int value) {
-	        int low = 0;
-	        int high = array.length;
-	        while (low < high) {
-	            final int mid = low + (high - low)/2;
-	            if (value <= array[mid]) {
-	                high = mid;
-	            } else {
-	                low = mid + 1;
-	            }
-	        }
-	        return low;
-	    }
+	public static int lowerBound(int[] array,  int value) {
+		int low = 0;
+		int high = array.length;
+		while (low < high) {
+			final int mid = low + (high - low)/2;
+			if (value <= array[mid]) {
+				high = mid;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return low;
+	}
 
-	   public static int upperBound(int[] array, int value) {
-	        int low = 0;
-	        int high = array.length;
-	        while (low < high) {
-	            final int mid = low + (high - low)/2;
-	            if (value >= array[mid]) {
-	                low = mid + 1;
-	            } else {
-	                high = mid;
-	            }
-	        }
-	        return low;
-	    }
+	public static int upperBound(int[] array, int value) {
+		int low = 0;
+		int high = array.length;
+		while (low < high) {
+			final int mid = low + (high - low)/2;
+			if (value >= array[mid]) {
+				low = mid + 1;
+			} else {
+				high = mid;
+			}
+		}
+		return low;
+	}
 
 }
---
+++ java_codingtest/src/baekjoon/baek2467.java
@@ -0,0 +1,69 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+//투포인터를 이용하라고 대놓고 광고한 문제
+//시간복잡도 O(N)
+
+//시작 min 값이 음수일때를 생각 못해서 한참을 맞왜틀...
+
+//http://boj.kr/6570cc47ce1744748563f71d555fadc7
+public class baek2467 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[] list = new int[N];
+
+		st=new StringTokenizer(br.readLine());
+
+		for(int i=0;i<N;i++)
+			list[i]=Integer.parseInt(st.nextToken());
+
+
+		//투포인터
+		int start=0;
+		int end=N-1;
+
+		int min = Math.abs(list[start]+list[end]); //최소값
+
+		int ans1=list[start];
+		int ans2=list[end];
+
+		while(start<end) {//포인터가 만나면 종료
+			int sum = list[start]+list[end]; //최대값이 20억을 넘지 않으므로 int 가능
+
+			if(sum>0) {
+				if(sum<min) {
+					ans1=list[start];
+					ans2=list[end];
+					min=sum;
+				}
+				end--;
+			}else if(sum<0) {
+				sum= -sum;
+				if(sum<min) {
+					ans1=list[start];
+					ans2=list[end];
+					min=sum;
+				}
+				start++;
+			}else {//sum==0
+				ans1=list[start];
+				ans2=list[end];
+				break;
+			}
+		}
+		System.out.print(ans1+" "+ans2);
+	}
+
+}
